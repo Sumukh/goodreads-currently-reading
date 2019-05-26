1ba708680a7e486e4310a27d77798b0093d515e4
Sumukh Sridhara <sumukh1@gmail.com>
2019-05-25 21:43:15 -0700

    currently

--- index.py
+++ index.py
@@ -7,11 +7,6 @@ css = '<link rel="stylesheet" href="/css/style.css" />'
 
 @app.route('/')
 def index():
-    return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Visit the <a href='./about'>about</a> page or view the <a href='%s'>source code</a>.</p>" % (css, source), mimetype='text/html')
-
-@app.route('/currently-reading.json')
-def currently_reading():
-    # shelf="https://www.goodreads.com/review/list/29316435.xml?shelf=currently-reading&key=MkLJEO345pEejIPqyrcBUw&v=2"
     data = requests.get('https://currently-reads.now.sh/reading/29316435/json').json()
     simple_data = [{
         'title': book['book'][0]["title"][0],
@@ -26,4 +21,3 @@ def currently_reading():
         'authors': [a["author"][0]["name"][0] for a in book['book'][0]["authors"]]
      } for book in data]
     return jsonify(simple_data)
-
