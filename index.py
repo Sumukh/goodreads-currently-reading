214ab11e75c1bbb571be49b45eea10ea402c141c
Sumukh Sridhara <sumukh1@gmail.com>
2019-06-01 23:16:06 -0700

    add precise days ago

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 from flask import Flask, Response, redirect, __version__, jsonify
 import requests
 from datetime import datetime, timedelta
-import arrow
 import humanize
 
 app = Flask(__name__)
@@ -9,14 +8,14 @@ app = Flask(__name__)
 def parse_goodreads_time(time_str):
     return datetime.strptime(time_str[0:-10] + time_str[-4:], '%a %b %d %H:%M:%S %Y')
 
-def days_ago(time_str):
+def days_ago(time_str, cloak_recent=True):
     if not time_str:
         return ''
-    if (datetime.now() - parse_goodreads_time(time_str)) < timedelta(days=7):
+    if cloak_recent and (datetime.now() - parse_goodreads_time(time_str)) < timedelta(days=7):
         return 'this week'
-    if (datetime.now() - parse_goodreads_time(time_str)) < timedelta(days=31):
+    if cloak_recent and (datetime.now() - parse_goodreads_time(time_str)) < timedelta(days=31):
         return 'this month'
-    if (datetime.now() - parse_goodreads_time(time_str)) > timedelta(days=180):
+    if cloak_recent and (datetime.now() - parse_goodreads_time(time_str)) > timedelta(days=180):
         return 'a while ago'
     return humanize.naturaltime(datetime.now() - parse_goodreads_time(time_str))
 
@@ -28,11 +27,14 @@ def currently_reading(goodreads_user_id):
         'image': book['book'][0]["image_url"][0],
         'started_at': book['started_at'][0],
         'started_at_ago': days_ago(book['started_at'][0]),
+        'started_at_ago_actual': days_ago(book['started_at'][0], cloak_recent=False),
         'read_at': book['read_at'][0],
         'read_at_ago': days_ago(book['read_at'][0]),
+        'read_at_ago_actual': days_ago(book['read_at'][0], cloak_recent=False),
         'read_count': book['read_count'][0],
         'date_added': book['date_added'][0],
         'date_added_ago': days_ago(book['date_added'][0]),
+        'date_added_ago_actual': days_ago(book['date_added'][0], cloak_recent=False),
         'rating': book['rating'][0],
         'url': book['book'][0]["link"][0],
         'description': book['book'][0]["description"][0],
